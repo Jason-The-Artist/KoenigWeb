361c662df24fa0635f6e0445400a79f70eb5955c
Jason <jm.enns04@gmail.com>
2023-09-13 23:54:35 +0200

    finishing nav

--- src/components/views/MainNav.vue
+++ src/components/views/MainNav.vue
@@ -1,23 +1,26 @@
 <template>
 <div class="outer-nav center-vertical">
   <div class="fullwidth">
-    <div class="relative center-vertical">
-      <div class="absolute center-horizontal nav center-vertical white-background" style="width: 100%">
-        <h2 class="nav-module center-horizontal logo-green" @click="click1">Nav 1. Element</h2>
-        <div class="nav-space"></div>
-        <h2 class="nav-module center-horizontal logo-green" @click="click2">Nav 2. Element</h2>
-        <div class="nav-space"></div>
-        <h2 class="nav-module center-horizontal logo-green" @click="click3">Nav 3. Element</h2>
-        <div class="nav-space"></div>
-      </div>
-      <div class="absolute nav-element display-flex round-corner" style="margin-left: 300px">
-        <img src="../../assets/logo_transparent.png" class="nav-logo">
-      </div>
-      <div class="absolute nav-element display-flex round-corner" style="margin-left: 70vw">
-        <div style="margin-left: 20px; margin-right: 20px">
-          <h2 class="logo-grey">Tel 0000-00000 Fax: 0000-00000</h2>
-          <h2 class="logo-grey">Email: test.email@gmail.com</h2>
+    <div>
+      <div class="display-flex fullwidth center-vertical">
+        <div class="white-background nav-height" style="width: 5%"></div>
+        <div class="nav-element nav-round-corner nav-logo-element">
+          <img src="../../assets/logo_transparent.png" class="nav-logo">
         </div>
+        <div class="full-center white-background nav-height" style="width: 45%">
+          <h2 class="nav-module center-horizontal logo-green" @click="click1">Nav 1. Element</h2>
+          <div class="nav-space"></div>
+          <h2 class="nav-module center-horizontal logo-green" @click="click2">Nav 2. Element</h2>
+          <div class="nav-space"></div>
+          <h2 class="nav-module center-horizontal logo-green" @click="click3">Nav 3. Element</h2>
+        </div>
+        <div class="nav-element center-horizontal nav-round-corner nav-info-element" style="width: 25%">
+          <div>
+            <h2 class="logo-grey nav-info-text">Tel 0000-00000</h2>
+            <h2 class="logo-grey nav-info-text">Email: test.email@gmail.com</h2>
+          </div>
+        </div>
+        <div class="white-background nav-height" style="width: 5%"></div>
       </div>
     </div>
   </div>
